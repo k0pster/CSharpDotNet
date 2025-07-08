408e8a023b855ff786a00998f37168d0097e98a2
k0pster <39728802+k0pster@users.noreply.github.com>
2025-07-08 12:42:04 +0200

    Add files via upload

---
+++ Learning ASP.NET Core MVC, Razor Pages, Web APIs & Other Foundations/razor-pages/Areas/HR/Pages/Index.cshtml.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace razor_pages.HR
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty(SupportsGet = true)]
+        public string? Name { get; set; }
+        [BindProperty(SupportsGet=true)]
+        public int? ID { get; set; }
+        public void OnGet()
+        {
+        }
+    }
+}
