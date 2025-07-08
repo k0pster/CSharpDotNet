e36f3af66e94183056b6b79b5d6f33db04bc5935
k0pster <39728802+k0pster@users.noreply.github.com>
2025-07-08 12:39:22 +0200

    Add files via upload

---
+++ Learning ASP.NET Core MVC, Razor Pages, Web APIs & Other Foundations/razor-pages/MessageHub.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.SignalR;
+
+public class MessageHub : Hub
+{
+    public async Task PostMessage(string message)
+    {
+        await Clients.All.SendAsync("ReceiveNewMessage", message);
+    }
+}
